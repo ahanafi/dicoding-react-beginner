30e16e89cc7b6f6bbfa1be494c46030b0d204953
Ahmad Hanafi <ahanafi.id@gmail.com>
2022-08-19 06:50:53 +0700

    Feat: add search bar

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -1,5 +1,7 @@
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react'
-import { Nav } from 'react-bootstrap';
+import { InputGroup, Button, Form, Nav } from 'react-bootstrap';
 import NavItem from './NavItem';
 
 const MenuBar = () => {
@@ -8,11 +10,22 @@ const MenuBar = () => {
       as='ul'
       variant="pills"
       defaultActiveKey="/home"
-      className='justify-content-start p-3 bg-white rounded-pill align-items-center mb-4'
+      className='p-3 bg-white rounded-pill align-items-center mb-4'
     >
       <NavItem text='All Notes' action='/' />
       <NavItem text='Archived Notes' action='/archive' />
       <NavItem text='Add New Note' action='/add-notes' />
+
+      <li className='nav-item ml-auto'>
+        <InputGroup>
+          <Form.Control
+            placeholder='Search notes here...'
+          />
+          <Button variant="outline-primary">
+            <FontAwesomeIcon icon={faSearch} />
+          </Button>
+        </InputGroup>
+      </li>
     </Nav>
   );
 }
