46c9e55eb3495b8ba8e892a34cf100da7de45316
Ahmad Hanafi <ahanafi.id@gmail.com>
2022-08-19 06:17:56 +0700

    Feat: add active/inactive menu

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button, Nav } from 'react-bootstrap';
+import { Nav } from 'react-bootstrap';
 import NavItem from './NavItem';
 
 const MenuBar = () => {
@@ -8,17 +8,11 @@ const MenuBar = () => {
       as='ul'
       variant="pills"
       defaultActiveKey="/home"
-      className='justify-content-between p-3 bg-white rounded-pill align-items-center mb-4'
+      className='justify-content-start p-3 bg-white rounded-pill align-items-center mb-4'
     >
-      <div className='d-flex flex-row'>
-        <NavItem isActive='true' text='All Notes' action='/' />
-        <NavItem text='Archived Notes' action='/archive' />
-      </div>
-      <li className='nav-item ml-auto'>
-        <Button variant='primary' className='rounded-pill d-flex align-items-center px-3'>
-          Add Notes
-        </Button>
-      </li>
+      <NavItem text='All Notes' action='/' />
+      <NavItem text='Archived Notes' action='/archive' />
+      <NavItem text='Add New Note' action='/add-notes' />
     </Nav>
   );
 }
--- src/components/NavItem.js
+++ src/components/NavItem.js
@@ -1,19 +1,16 @@
 import React from 'react'
 import { Nav } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
-const NavItem = ({ text, action, isActive = false}) => {
+const NavItem = ({ text, action}) => {
   return (
     <Nav.Item as='li'>
-      <Nav.Link
-        as={Link}
+      <NavLink
         to={action}
-        className={
-          `rounded-pill note-link d-flex align-items-center px-2 px-md-3 mr-0 mr-md-2 ${isActive ? 'active' : ''}`
-        }
+        className={`nav-link rounded-pill note-link d-flex align-items-center px-2 px-md-3 mr-0 mr-md-2`}
         >
         {text}
-      </Nav.Link>
+      </NavLink>
     </Nav.Item>
   )
 }
